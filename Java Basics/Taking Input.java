6c10f52a0797a577d266635c7ad6628daa91f4b5
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-09-17 03:00:18 +0530

    Create Taking Input.java

---
+++ Java Basics/Taking Input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+        float f = sc.nextFloat();
+        int ff = (int) Math.floor(f);; // To Store floor of float variable f
+
+        
+
+        System.out.println(s);
+        System.out.println(n);
+        System.out.println(ff);
+    
+    }
+}
