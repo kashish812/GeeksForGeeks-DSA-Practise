afaf7ae7e0f4fc39b5b41bb0dac74d2bf1a27f3a
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-09-24 23:55:16 +0530

    Create 04-The Else If Statement.java

---
+++ Java Basics/if-else - Decision  Making/04-The Else If Statement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void utility(int number) {
+        if(number<10){
+            System.out.println("Small");
+        }
+        else if(number>100){
+            System.out.println("Big");
+        }
+        else if(number>10&&number<100){
+            System.out.println("Number");
+        }
+    
+    }
+}
