64730c50d0843861f14abb10025eeaee7f9481a5
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-09-24 23:58:58 +0530

    Add leap year checking function in Solution class

---
+++ Java Basics/if-else - Decision  Making/08-Leap Year.java
@@ -0,0 +1,10 @@
+class Solution {
+    static boolean checkYear(int n) {
+        if(n%400==0||n%4==0&&n%100!=0){
+            return true;
+        }
+        return false;
+        
+        
+    }
+}
