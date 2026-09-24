57f7d4687ac421c1fefb177c9cc37a174082f4aa
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-09-24 23:52:17 +0530

    Add if-else statement example in Java

---
+++ Java Basics/if-else - Decision  Making/03-The Else Statement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>100){
+            System.out.println("Big");
+        }
+        else{
+            System.out.println("Small");
+        }
+
+        
+    }
+}
